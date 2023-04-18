a65c8ea74f65eefeb081e8fc8f0721fcf82c8b96
psantos2018 <psantos@tdkportal.com>
2023-04-18 20:16:59 +0100

    Initial commit

---
+++ src/com/hedima/modelo/Empleado.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Empleado {
+    private String nombre;
+    private double sueldo;
+    private LocalDate fNacimiento;
+
+    public String mostrarinfo(){
+        return "Nombre:"+
+    }
+
+}
